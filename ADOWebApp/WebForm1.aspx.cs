b5a7c015bb1be43c8b145fcdd39faf6218ea0a18
vjpal3 <vrishalipal@yahoo.com>
2020-02-17 17:05:51 -0500

    CRUD operations

--- ADOWebApp/WebForm1.aspx.cs
+++ ADOWebApp/WebForm1.aspx.cs
@@ -13,7 +13,49 @@ namespace ADOWebApp
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            
+            string constr = ConfigurationManager.ConnectionStrings["constr"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(constr))
+            {
+                string cmdText = "Insert into People values('Leila', 'Jones')";
+                var cmd = new SqlCommand(cmdText, con);
+                con.Open();
+                int totalRows = cmd.ExecuteNonQuery();
+                Response.Write("Total Rows inserted: " + totalRows + "<br />" );
+
+                cmdText = "select * from tblEmployee";
+                cmd = new SqlCommand(cmdText, con);
+                using (var reader = cmd.ExecuteReader())
+                {
+                    GridView1.DataSource = reader;
+                    GridView1.DataBind();
+                }
+
+
+                cmdText = "Select count(Id) from People";
+                cmd = new SqlCommand(cmdText, con);
+
+                ////use ExecuteScalar() when retrieving single value, more effiecient than ExecuteReader
+                var scalarReader = cmd.ExecuteScalar();
+                totalRows = (int)scalarReader;
+                Response.Write("Total Rows in People table: " + totalRows + "<br />");
+                //cmd.Dispose();
+                
+
+                cmdText = "Update People set FirstName='Maggie' where LastName='Coote'";
+                cmd = new SqlCommand(cmdText, con);
+                totalRows = cmd.ExecuteNonQuery();
+                Response.Write("Total Rows updated: " + totalRows + "<br />");
+
+                cmdText = "Delete from People where Id=17";
+                cmd = new SqlCommand(cmdText, con);
+                
+                //cmd.Connection = con;
+                //con.Open();
+                totalRows = cmd.ExecuteNonQuery();
+                Response.Write("Total Rows deleted: " + totalRows);
+
+            }
+
         }
 
         protected void Button1_Click(object sender, EventArgs e)
